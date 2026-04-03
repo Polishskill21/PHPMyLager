aa598e9f09f52a4e546880bf837b1436f6c90e91
Polishskill21 <rafalmiszkiewicz6@gmail.com>
2026-04-03 23:38:51 +0200

    fix: using roles for granular api controll

--- app/routes/api.php
+++ app/routes/api.php
@@ -6,5 +6,22 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/status', [SystemController::class, 'status']);
 
-//products route
-Route::apiResource('products', ProductController::class);
+//auth:sanctum could be used later
+Route::middleware('auth')->group(function () {
+
+    // All roles can read
+    Route::get('products',      [ProductController::class, 'index'])->name('products.index');
+    Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
+
+    // Admin + writer can create and update
+    Route::middleware('role:admin,writer')->group(function () {
+        Route::post('products',           [ProductController::class, 'store'])->name('products.store');
+        Route::put('products/{product}',  [ProductController::class, 'update'])->name('products.update');
+        Route::patch('products/{product}',[ProductController::class, 'update']);
+    });
+
+    // Admin only can delete
+    Route::middleware('role:admin')->group(function () {
+        Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+    });
+});
