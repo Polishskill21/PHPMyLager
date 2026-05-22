d6dfca11c9d493ef397bbd6d0e24af4f55bd9d5a
Polishskill21 <rafalmiszkiewicz6@gmail.com>
2026-05-22 17:42:23 +0200

    feat: seeding database with new data

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -45,43 +45,43 @@ class DatabaseSeeder extends Seeder
 
         // 3. Seed Artikel
         DB::table('artikel')->insertOrIgnore([
-            ['pArtikelNr' => 10004, 'bezeichnung' => 'Handlupe 90mm', 'fWgNr' => 4, 'ekPreis' => 10.00, 'vkPreis' => 18.00, 'bestand' => 300, 'meldeBest' => 100],
-            ['pArtikelNr' => 10005, 'bezeichnung' => 'Lupe 90mm', 'fWgNr' => 4, 'ekPreis' => 5.00, 'vkPreis' => 9.00, 'bestand' => 1010, 'meldeBest' => 400],
-            ['pArtikelNr' => 10028, 'bezeichnung' => 'Pruefschraubendreher-Set', 'fWgNr' => 2, 'ekPreis' => 13.00, 'vkPreis' => 25.00, 'bestand' => 680, 'meldeBest' => 210],
-            ['pArtikelNr' => 10030, 'bezeichnung' => 'Schraubendreher 1.5mm', 'fWgNr' => 2, 'ekPreis' => 1.00, 'vkPreis' => 2.00, 'bestand' => 290, 'meldeBest' => 100],
-            ['pArtikelNr' => 10031, 'bezeichnung' => 'Schraubendreher 1.8mm', 'fWgNr' => 2, 'ekPreis' => 1.00, 'vkPreis' => 2.00, 'bestand' => 220, 'meldeBest' => 100],
-            ['pArtikelNr' => 10034, 'bezeichnung' => 'Schraubendreher 3.0mm', 'fWgNr' => 2, 'ekPreis' => 1.00, 'vkPreis' => 2.00, 'bestand' => 300, 'meldeBest' => 100],
-            ['pArtikelNr' => 10044, 'bezeichnung' => 'Stahllaubsaege', 'fWgNr' => 3, 'ekPreis' => 5.00, 'vkPreis' => 10.00, 'bestand' => 1250, 'meldeBest' => 300],
-            ['pArtikelNr' => 10049, 'bezeichnung' => 'Laubsaegeblaetter (12er Set)', 'fWgNr' => 3, 'ekPreis' => 2.00, 'vkPreis' => 4.00, 'bestand' => 2400, 'meldeBest' => 400],
-            ['pArtikelNr' => 10050, 'bezeichnung' => 'Universal-Hobbysaege', 'fWgNr' => 3, 'ekPreis' => 6.00, 'vkPreis' => 11.00, 'bestand' => 1350, 'meldeBest' => 200],
-            ['pArtikelNr' => 10056, 'bezeichnung' => 'Isolier-Abstreifzaengleinchen', 'fWgNr' => 1, 'ekPreis' => 14.00, 'vkPreis' => 20.00, 'bestand' => 2400, 'meldeBest' => 250],
-            ['pArtikelNr' => 10057, 'bezeichnung' => 'Adernendhuelsen-Zaengle', 'fWgNr' => 1, 'ekPreis' => 17.00, 'vkPreis' => 31.00, 'bestand' => 1750, 'meldeBest' => 220],
-            ['pArtikelNr' => 10058, 'bezeichnung' => 'Universal-Kabelzange', 'fWgNr' => 1, 'ekPreis' => 6.00, 'vkPreis' => 12.00, 'bestand' => 1900, 'meldeBest' => 300],
-            ['pArtikelNr' => 10059, 'bezeichnung' => 'Schraubendreher-Set', 'fWgNr' => 2, 'ekPreis' => 11.00, 'vkPreis' => 21.00, 'bestand' => 1800, 'meldeBest' => 180],
-            ['pArtikelNr' => 10062, 'bezeichnung' => 'Pozidriv-Schraubendreher', 'fWgNr' => 2, 'ekPreis' => 3.00, 'vkPreis' => 5.00, 'bestand' => 2850, 'meldeBest' => 200],
-            ['pArtikelNr' => 10068, 'bezeichnung' => 'Elektronik-Seitenschneider', 'fWgNr' => 1, 'ekPreis' => 4.00, 'vkPreis' => 7.00, 'bestand' => 750, 'meldeBest' => 150],
-            ['pArtikelNr' => 10069, 'bezeichnung' => 'Elektronik-Flachzange', 'fWgNr' => 1, 'ekPreis' => 4.00, 'vkPreis' => 7.00, 'bestand' => 2800, 'meldeBest' => 250],
-            ['pArtikelNr' => 10070, 'bezeichnung' => 'Elektronik-Halbrundzange', 'fWgNr' => 1, 'ekPreis' => 4.00, 'vkPreis' => 7.00, 'bestand' => 1950, 'meldeBest' => 400],
-            ['pArtikelNr' => 10071, 'bezeichnung' => 'Loch- und Oesenzange', 'fWgNr' => 1, 'ekPreis' => 13.00, 'vkPreis' => 25.00, 'bestand' => 2540, 'meldeBest' => 350],
-            ['pArtikelNr' => 10075, 'bezeichnung' => 'Edelstahl-Flachzange', 'fWgNr' => 1, 'ekPreis' => 7.00, 'vkPreis' => 12.00, 'bestand' => 150, 'meldeBest' => 300],
-            ['pArtikelNr' => 10076, 'bezeichnung' => 'Automatik-Abisolierzange', 'fWgNr' => 1, 'ekPreis' => 5.00, 'vkPreis' => 9.00, 'bestand' => 100, 'meldeBest' => 250],
-            ['pArtikelNr' => 10080, 'bezeichnung' => 'Telefonzange 200mm', 'fWgNr' => 1, 'ekPreis' => 6.00, 'vkPreis' => 11.00, 'bestand' => 1950, 'meldeBest' => 200],
-            ['pArtikelNr' => 10081, 'bezeichnung' => 'Mehrzweckzange', 'fWgNr' => 1, 'ekPreis' => 19.00, 'vkPreis' => 35.00, 'bestand' => 4500, 'meldeBest' => 200],
-            ['pArtikelNr' => 10086, 'bezeichnung' => 'Multifunktions-Crimpzange', 'fWgNr' => 1, 'ekPreis' => 40.00, 'vkPreis' => 75.00, 'bestand' => 1150, 'meldeBest' => 150],
-            ['pArtikelNr' => 11058, 'bezeichnung' => 'Spezial-Bauschubkarre', 'fWgNr' => 4, 'ekPreis' => 60.00, 'vkPreis' => 114.00, 'bestand' => 450, 'meldeBest' => 250],
-            ['pArtikelNr' => 11062, 'bezeichnung' => 'Durchwurfsieb verzinkt 100x60cm', 'fWgNr' => 4, 'ekPreis' => 40.00, 'vkPreis' => 76.00, 'bestand' => 550, 'meldeBest' => 250],
-            ['pArtikelNr' => 12345, 'bezeichnung' => 'Zange', 'fWgNr' => 1, 'ekPreis' => 12.00, 'vkPreis' => 20.00, 'bestand' => 100, 'meldeBest' => 50],
-            ['pArtikelNr' => 70001, 'bezeichnung' => 'Werkzeugkasten Universal', 'fWgNr' => 4, 'ekPreis' => 149.00, 'vkPreis' => 283.00, 'bestand' => 120, 'meldeBest' => 50],
-            ['pArtikelNr' => 71001, 'bezeichnung' => 'Schlagbohrmaschine', 'fWgNr' => 4, 'ekPreis' => 63.00, 'vkPreis' => 120.00, 'bestand' => 155, 'meldeBest' => 50],
-            ['pArtikelNr' => 71002, 'bezeichnung' => 'Bohrerset fuer Holz/Metall/Stein', 'fWgNr' => 4, 'ekPreis' => 4.00, 'vkPreis' => 7.00, 'bestand' => 135, 'meldeBest' => 50],
-            ['pArtikelNr' => 71003, 'bezeichnung' => 'Bit-Steckschluesselsatz', 'fWgNr' => 4, 'ekPreis' => 3.00, 'vkPreis' => 6.00, 'bestand' => 124, 'meldeBest' => 50],
-            ['pArtikelNr' => 71004, 'bezeichnung' => 'Schlosserhammer', 'fWgNr' => 4, 'ekPreis' => 4.00, 'vkPreis' => 7.00, 'bestand' => 90, 'meldeBest' => 80],
-            ['pArtikelNr' => 72102, 'bezeichnung' => 'Wasserpumpenzange 240mm', 'fWgNr' => 1, 'ekPreis' => 4.00, 'vkPreis' => 7.00, 'bestand' => 122, 'meldeBest' => 20],
-            ['pArtikelNr' => 72250, 'bezeichnung' => 'Wasserwaage 400mm', 'fWgNr' => 4, 'ekPreis' => 4.00, 'vkPreis' => 8.00, 'bestand' => 90, 'meldeBest' => 40],
-            ['pArtikelNr' => 72255, 'bezeichnung' => 'Universalsaege', 'fWgNr' => 3, 'ekPreis' => 5.00, 'vkPreis' => 10.00, 'bestand' => 95, 'meldeBest' => 40],
-            ['pArtikelNr' => 72256, 'bezeichnung' => 'Saegeblatt Holz', 'fWgNr' => 3, 'ekPreis' => 1.00, 'vkPreis' => 1.00, 'bestand' => 124, 'meldeBest' => 40],
-            ['pArtikelNr' => 72257, 'bezeichnung' => 'Saegeblatt Metall', 'fWgNr' => 3, 'ekPreis' => 2.00, 'vkPreis' => 3.00, 'bestand' => 132, 'meldeBest' => 40],
-            ['pArtikelNr' => 74001, 'bezeichnung' => 'Kasten 75x45', 'fWgNr' => 4, 'ekPreis' => 7.00, 'vkPreis' => 13.00, 'bestand' => 105, 'meldeBest' => 40],
+            ['pArtikelNr' => 10004, 'bezeichnung' => 'Handlupe 90mm', 'fWgNr' => 4, 'ekPreis' => 10.00, 'vkPreis' => 18.00, 'bestand' => 300, 'meldeBest' => 100, 'lagerplatz' => 'D01-12A'],
+            ['pArtikelNr' => 10005, 'bezeichnung' => 'Lupe 90mm', 'fWgNr' => 4, 'ekPreis' => 5.00, 'vkPreis' => 9.00, 'bestand' => 1010, 'meldeBest' => 400, 'lagerplatz' => 'D01-12B'],
+            ['pArtikelNr' => 10028, 'bezeichnung' => 'Pruefschraubendreher-Set', 'fWgNr' => 2, 'ekPreis' => 13.00, 'vkPreis' => 25.00, 'bestand' => 680, 'meldeBest' => 210, 'lagerplatz' => 'B04-02C'],
+            ['pArtikelNr' => 10030, 'bezeichnung' => 'Schraubendreher 1.5mm', 'fWgNr' => 2, 'ekPreis' => 1.00, 'vkPreis' => 2.00, 'bestand' => 290, 'meldeBest' => 100, 'lagerplatz' => 'B01-01A'],
+            ['pArtikelNr' => 10031, 'bezeichnung' => 'Schraubendreher 1.8mm', 'fWgNr' => 2, 'ekPreis' => 1.00, 'vkPreis' => 2.00, 'bestand' => 220, 'meldeBest' => 100, 'lagerplatz' => 'B01-01B'],
+            ['pArtikelNr' => 10034, 'bezeichnung' => 'Schraubendreher 3.0mm', 'fWgNr' => 2, 'ekPreis' => 1.00, 'vkPreis' => 2.00, 'bestand' => 300, 'meldeBest' => 100, 'lagerplatz' => 'B01-02A'],
+            ['pArtikelNr' => 10044, 'bezeichnung' => 'Stahllaubsaege', 'fWgNr' => 3, 'ekPreis' => 5.00, 'vkPreis' => 10.00, 'bestand' => 1250, 'meldeBest' => 300, 'lagerplatz' => 'C02-05A'],
+            ['pArtikelNr' => 10049, 'bezeichnung' => 'Laubsaegeblaetter (12er Set)', 'fWgNr' => 3, 'ekPreis' => 2.00, 'vkPreis' => 4.00, 'bestand' => 2400, 'meldeBest' => 400, 'lagerplatz' => 'C02-05E'],
+            ['pArtikelNr' => 10050, 'bezeichnung' => 'Universal-Hobbysaege', 'fWgNr' => 3, 'ekPreis' => 6.00, 'vkPreis' => 11.00, 'bestand' => 1350, 'meldeBest' => 200, 'lagerplatz' => 'C03-01B'],
+            ['pArtikelNr' => 10056, 'bezeichnung' => 'Isolier-Abstreifzaengleinchen', 'fWgNr' => 1, 'ekPreis' => 14.00, 'vkPreis' => 20.00, 'bestand' => 2400, 'meldeBest' => 250, 'lagerplatz' => 'A01-03B'],
+            ['pArtikelNr' => 10057, 'bezeichnung' => 'Adernendhuelsen-Zaengle', 'fWgNr' => 1, 'ekPreis' => 17.00, 'vkPreis' => 31.00, 'bestand' => 1750, 'meldeBest' => 220, 'lagerplatz' => 'A01-04C'],
+            ['pArtikelNr' => 10058, 'bezeichnung' => 'Universal-Kabelzange', 'fWgNr' => 1, 'ekPreis' => 6.00, 'vkPreis' => 12.00, 'bestand' => 1900, 'meldeBest' => 300, 'lagerplatz' => 'A02-01A'],
+            ['pArtikelNr' => 10059, 'bezeichnung' => 'Schraubendreher-Set', 'fWgNr' => 2, 'ekPreis' => 11.00, 'vkPreis' => 21.00, 'bestand' => 1800, 'meldeBest' => 180, 'lagerplatz' => 'B05-01D'],
+            ['pArtikelNr' => 10062, 'bezeichnung' => 'Pozidriv-Schraubendreher', 'fWgNr' => 2, 'ekPreis' => 3.00, 'vkPreis' => 5.00, 'bestand' => 2850, 'meldeBest' => 200, 'lagerplatz' => 'B02-03C'],
+            ['pArtikelNr' => 10068, 'bezeichnung' => 'Elektronik-Seitenschneider', 'fWgNr' => 1, 'ekPreis' => 4.00, 'vkPreis' => 7.00, 'bestand' => 750, 'meldeBest' => 150, 'lagerplatz' => 'A03-02B'],
+            ['pArtikelNr' => 10069, 'bezeichnung' => 'Elektronik-Flachzange', 'fWgNr' => 1, 'ekPreis' => 4.00, 'vkPreis' => 7.00, 'bestand' => 2800, 'meldeBest' => 250, 'lagerplatz' => 'A03-02C'],
+            ['pArtikelNr' => 10070, 'bezeichnung' => 'Elektronik-Halbrundzange', 'fWgNr' => 1, 'ekPreis' => 4.00, 'vkPreis' => 7.00, 'bestand' => 1950, 'meldeBest' => 400, 'lagerplatz' => 'A03-02D'],
+            ['pArtikelNr' => 10071, 'bezeichnung' => 'Loch- und Oesenzange', 'fWgNr' => 1, 'ekPreis' => 13.00, 'vkPreis' => 25.00, 'bestand' => 2540, 'meldeBest' => 350, 'lagerplatz' => 'A04-05E'],
+            ['pArtikelNr' => 10075, 'bezeichnung' => 'Edelstahl-Flachzange', 'fWgNr' => 1, 'ekPreis' => 7.00, 'vkPreis' => 12.00, 'bestand' => 150, 'meldeBest' => 300, 'lagerplatz' => 'A05-01A'],
+            ['pArtikelNr' => 10076, 'bezeichnung' => 'Automatik-Abisolierzange', 'fWgNr' => 1, 'ekPreis' => 5.00, 'vkPreis' => 9.00, 'bestand' => 100, 'meldeBest' => 250, 'lagerplatz' => 'A05-02B'],
+            ['pArtikelNr' => 10080, 'bezeichnung' => 'Telefonzange 200mm', 'fWgNr' => 1, 'ekPreis' => 6.00, 'vkPreis' => 11.00, 'bestand' => 1950, 'meldeBest' => 200, 'lagerplatz' => 'A02-04C'],
+            ['pArtikelNr' => 10081, 'bezeichnung' => 'Mehrzweckzange', 'fWgNr' => 1, 'ekPreis' => 19.00, 'vkPreis' => 35.00, 'bestand' => 4500, 'meldeBest' => 200, 'lagerplatz' => 'A02-05A'],
+            ['pArtikelNr' => 10086, 'bezeichnung' => 'Multifunktions-Crimpzange', 'fWgNr' => 1, 'ekPreis' => 40.00, 'vkPreis' => 75.00, 'bestand' => 1150, 'meldeBest' => 150, 'lagerplatz' => 'A06-01B'],
+            ['pArtikelNr' => 11058, 'bezeichnung' => 'Spezial-Bauschubkarre', 'fWgNr' => 4, 'ekPreis' => 60.00, 'vkPreis' => 114.00, 'bestand' => 450, 'meldeBest' => 250, 'lagerplatz' => 'E01-01A'],
+            ['pArtikelNr' => 11062, 'bezeichnung' => 'Durchwurfsieb verzinkt 100x60cm', 'fWgNr' => 4, 'ekPreis' => 40.00, 'vkPreis' => 76.00, 'bestand' => 550, 'meldeBest' => 250, 'lagerplatz' => 'E02-01A'],
+            ['pArtikelNr' => 12345, 'bezeichnung' => 'Zange', 'fWgNr' => 1, 'ekPreis' => 12.00, 'vkPreis' => 20.00, 'bestand' => 100, 'meldeBest' => 50,  'lagerplatz' => 'A01-01A'],
+            ['pArtikelNr' => 70001, 'bezeichnung' => 'Werkzeugkasten Universal', 'fWgNr' => 4, 'ekPreis' => 149.00, 'vkPreis' => 283.00, 'bestand' => 120, 'meldeBest' => 50, 'lagerplatz' => 'D05-04A'],
+            ['pArtikelNr' => 71001, 'bezeichnung' => 'Schlagbohrmaschine', 'fWgNr' => 4, 'ekPreis' => 63.00, 'vkPreis' => 120.00, 'bestand' => 155, 'meldeBest' => 50, 'lagerplatz' => 'D06-01B'],
+            ['pArtikelNr' => 71002, 'bezeichnung' => 'Bohrerset fuer Holz/Metall/Stein', 'fWgNr' => 4, 'ekPreis' => 4.00, 'vkPreis' => 7.00, 'bestand' => 135, 'meldeBest' => 50, 'lagerplatz' => 'D06-02A'],
+            ['pArtikelNr' => 71003, 'bezeichnung' => 'Bit-Steckschluesselsatz', 'fWgNr' => 4, 'ekPreis' => 3.00, 'vkPreis' => 6.00, 'bestand' => 124, 'meldeBest' => 50, 'lagerplatz' => 'D06-02B'],
+            ['pArtikelNr' => 71004, 'bezeichnung' => 'Schlosserhammer', 'fWgNr' => 4, 'ekPreis' => 4.00, 'vkPreis' => 7.00, 'bestand' => 90, 'meldeBest' => 80, 'lagerplatz' => 'D03-01C'],
+            ['pArtikelNr' => 72102, 'bezeichnung' => 'Wasserpumpenzange 240mm', 'fWgNr' => 1, 'ekPreis' => 4.00, 'vkPreis' => 7.00, 'bestand' => 122, 'meldeBest' => 20, 'lagerplatz' => 'A02-02B'],
+            ['pArtikelNr' => 72250, 'bezeichnung' => 'Wasserwaage 400mm', 'fWgNr' => 4, 'ekPreis' => 4.00, 'vkPreis' => 8.00, 'bestand' => 90, 'meldeBest' => 40, 'lagerplatz' => 'D02-01A'],
+            ['pArtikelNr' => 72255, 'bezeichnung' => 'Universalsaege', 'fWgNr' => 3, 'ekPreis' => 5.00, 'vkPreis' => 10.00, 'bestand' => 95, 'meldeBest' => 40, 'lagerplatz' => 'C01-01A'],
+            ['pArtikelNr' => 72256, 'bezeichnung' => 'Saegeblatt Holz', 'fWgNr' => 3, 'ekPreis' => 1.00, 'vkPreis' => 1.00, 'bestand' => 124, 'meldeBest' => 40, 'lagerplatz' => 'C01-02A'],
+            ['pArtikelNr' => 72257, 'bezeichnung' => 'Saegeblatt Metall', 'fWgNr' => 3, 'ekPreis' => 2.00, 'vkPreis' => 3.00, 'bestand' => 132, 'meldeBest' => 40, 'lagerplatz' => 'C01-02B'],
+            ['pArtikelNr' => 74001, 'bezeichnung' => 'Kasten 75x45', 'fWgNr' => 4, 'ekPreis' => 7.00, 'vkPreis' => 13.00, 'bestand' => 105, 'meldeBest' => 40, 'lagerplatz' => 'D04-01A'],
         ]);
 
         // 4. Seed Kunden
@@ -115,6 +115,31 @@ class DatabaseSeeder extends Seeder
             ['pAufPosNr' => 6, 'fAufNr' => 22336, 'fArtikelNr' => 10004, 'aufMenge' => 40, 'kaufPreis' => 18.00],
             ['pAufPosNr' => 7, 'fAufNr' => 22337, 'fArtikelNr' => 10069, 'aufMenge' => 5,  'kaufPreis' =>  7.00],
             ['pAufPosNr' => 8, 'fAufNr' => 22337, 'fArtikelNr' => 10070, 'aufMenge' => 5,  'kaufPreis' =>  7.00],
+        ]);
+
+        // 7. Seed Lieferanten
+        DB::table('lieferanten')->insertOrIgnore([
+            ['pLiefNr' => 5001, 'name' => 'Remscheid Werkzeuge GmbH', 'strasse' => 'Industriepark Nord 4', 'plz' => 42853, 'ort' => 'Remscheid', 'email' => 'vertrieb@remscheid-tools.de', 'telefon' => '+49 2191 555120', 'created_at' => '2026-01-15 08:00:00', 'updated_at' => '2026-01-15 08:00:00'],
+            ['pLiefNr' => 5002, 'name' => 'Sheffield Steel Co.', 'strasse' => '22 Ironworks Lane', 'plz' => 54321, 'ort' => 'Sheffield', 'email' => 'orders@sheffieldsteel.co.uk', 'telefon' => '+44 114 9620000', 'created_at' => '2026-02-10 09:30:00', 'updated_at' => '2026-02-10 09:30:00'],
+            ['pLiefNr' => 5003, 'name' => 'Alpen Werkzeuge Import S.A.', 'strasse' => 'Rue du Commerce 77', 'plz' => 1005, 'ort' => 'Lausanne', 'email' => 'info@alpenimport.ch', 'telefon' => '+41 21 3456789', 'created_at' => '2026-04-01 14:15:00', 'updated_at' => '2026-04-01 14:15:00']
+        ]);
+
+        // 8. Seed Bestellkoepfe (Purchase Orders placed to Suppliers)
+        DB::table('bestellkoepfe')->insertOrIgnore([
+            ['pBestNr' => 80001, 'fLiefNr' => 5001, 'bestDat' => '2026-05-01 10:00:00', 'erwLieferDat' => '2026-05-08 14:00:00', 'status' => 'geliefert', 'created_at' => '2026-05-01 10:00:00', 'updated_at' => '2026-05-08 14:10:00'],
+            ['pBestNr' => 80002, 'fLiefNr' => 5002, 'bestDat' => '2026-05-15 11:30:00', 'erwLieferDat' => '2026-05-22 12:00:00', 'status' => 'bestellt', 'created_at' => '2026-05-15 11:30:00', 'updated_at' => '2026-05-22 09:00:00'],
+            ['pBestNr' => 80003, 'fLiefNr' => 5003, 'bestDat' => '2026-05-21 16:45:00', 'erwLieferDat' => '2026-05-28 16:00:00', 'status' => 'offen', 'created_at' => '2026-05-21 16:45:00', 'updated_at' => '2026-05-21 16:45:00']
+        ]);
+
+
+        // 9. Seed Bestellpositionen (Purchase Order Items mapping quantities)
+        DB::table('bestellpositionen')->insertOrIgnore([
+            ['pBestPosNr' => 101, 'fBestNr' => 80001, 'fArtikelNr' => 10059, 'bestMenge' => 100, 'gelieferteMenge' => 100, 'ekPreis' => 11.00], 
+            ['pBestPosNr' => 102, 'fBestNr' => 80001, 'fArtikelNr' => 10068, 'bestMenge' => 50, 'gelieferteMenge' => 50, 'ekPreis' => 4.00],
+            ['pBestPosNr' => 103, 'fBestNr' => 80002, 'fArtikelNr' => 10044, 'bestMenge' => 200, 'gelieferteMenge' => 150, 'ekPreis' => 4.80],
+            ['pBestPosNr' => 104, 'fBestNr' => 80002, 'fArtikelNr' => 10049, 'bestMenge' => 500, 'gelieferteMenge' => 500, 'ekPreis' => 1.85],
+            ['pBestPosNr' => 105, 'fBestNr' => 80003, 'fArtikelNr' => 10086, 'bestMenge' => 30, 'gelieferteMenge' => 0, 'ekPreis' => 38.50],
+            ['pBestPosNr' => 106, 'fBestNr' => 80003, 'fArtikelNr' => 71001, 'bestMenge' => 15, 'gelieferteMenge' => 0, 'ekPreis' => 60.00]
         ]);
     }
 }
