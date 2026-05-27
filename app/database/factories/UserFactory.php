7ff817c803321b5a4b322e7c2077a619039f6c36
Polishskill21 <rafalmiszkiewicz6@gmail.com>
2026-05-27 18:08:51 +0200

    fix: using const variables

--- app/database/factories/UserFactory.php
+++ app/database/factories/UserFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Factories;
+namespace Database\Factories\Auth;
 
 use App\Models\Auth\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -25,11 +25,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make(env('SEEDER_PASSWORD', 'password')),
-            'remember_token' => Str::random(10),
+            User::COL_NAME => fake()->name(),
+            User::COL_EMAIL => fake()->unique()->safeEmail(),
+            User::COL_EMAIL_VERIFIED_AT => now(),
+            User::COL_PASSWORD => static::$password ??= Hash::make(env('SEEDER_PASSWORD', 'password')),
+            User::COL_REMEMBER_TOKEN => Str::random(10),
         ];
     }
 
@@ -39,7 +39,7 @@ class UserFactory extends Factory
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+            User::COL_EMAIL_VERIFIED_AT => null,
         ]);
     }
 }
