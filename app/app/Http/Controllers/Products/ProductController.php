a8ef7dd401d099a677871a027b15692d702b79c6
Polishskill21 <rafalmiszkiewicz6@gmail.com>
2026-05-31 00:37:44 +0200

    fix: adjusted GET /products/{product}/stock-history  endpoint

--- app/app/Http/Controllers/Products/ProductController.php
+++ app/app/Http/Controllers/Products/ProductController.php
@@ -55,7 +55,7 @@ class ProductController extends Controller
      */
     public function stockHistory(Product $product): JsonResponse
     {
-        $history = $product->inventoryLogs()->with('user:id,name')->get();
+        $history = $product->inventoryLogs()->get();
         return $this->ok($history);
     }
 
