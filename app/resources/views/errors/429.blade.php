73116a9dac30a96120bfd22dfac1e26a27b38136
Polishskill21 <rafalmiszkiewicz6@gmail.com>
2026-06-20 18:09:26 +0200

    feat: error pages

---
+++ app/resources/views/errors/429.blade.php
@@ -0,0 +1,5 @@
+@extends('errors.layout')
+
+@section('code', '429')
+@section('title', 'Too many requests')
+@section('message', 'You have made too many requests. Please slow down and try again shortly.')
